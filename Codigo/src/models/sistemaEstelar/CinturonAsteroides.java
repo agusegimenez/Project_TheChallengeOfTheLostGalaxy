337979b19301875de697f0ad1f40c349563d2270
agusegimenez <agusegimenez@gmail.com>
2024-05-26 15:50:29 -0300

    fix errores PlanetaAliado

--- Codigo/src/models/sistemaEstelar/CinturonAsteroides.java
+++ Codigo/src/models/sistemaEstelar/CinturonAsteroides.java
@@ -1,5 +1,17 @@
 package models.sistemaEstelar;
 
+import java.util.Random;
+
 public class CinturonAsteroides {
+    private int cantidadAsteroides;
+
+    public int getCantidadAsteroides() {
+        return cantidadAsteroides;
+    }
+
+    public void setCantidadAsteroides() {
+        Random cantidad = new Random();
+        this.cantidadAsteroides = cantidad.nextInt(50)+1;
+    }
 
 }
--- Codigo/src/models/sistemaEstelar/PlanetaAliado.java
+++ Codigo/src/models/sistemaEstelar/PlanetaAliado.java
@@ -41,3 +41,52 @@ public class PlanetaAliado extends Planeta{
     };
 
 }
+=======
+package models.planeta;
+
+import models.Nave.Nave;
+import models.equipamiento.Arma;
+import models.equipamiento.Escudo;
+
+public class PlanetaAliado extends Planeta{
+    public PlanetaAliado(String idPlaneta, int costoDeCombustible) {
+        super(idPlaneta, costoDeCombustible);
+    }
+
+    @Override
+    public boolean soyHostil() {
+        return (false);
+    }
+    @Override
+    public boolean soyNeutral() {
+        return (false);
+    }
+    @Override
+    public boolean soyAliado() {
+        return (true);
+    }
+    public Enemigo getEnemigo() {
+        throw new IllegalArgumentException("no es un Planeta Hostil");
+    }
+    public boolean tieneTesoro() {
+        return(false);
+    }
+    @Override
+    public Arma getArma() {
+        throw new IllegalArgumentException("No soy un Planeta Neutral");
+    }
+    @Override
+    public Escudo getEscudo() {
+        throw new IllegalArgumentException("No soy un Planeta Neutral");
+    }
+
+    private void repararNave(Nave nave){
+
+    }
+
+    private void comprarInformacion(int uadeCoins){
+
+    };
+
+}
+>>>>>>> Stashed changes:Codigo/src/models/planeta/PlanetaAliado.java
