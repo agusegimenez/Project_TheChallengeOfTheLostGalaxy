30581436d27ceb5cf0e0b9c4950e8f69b2cb31b9
agusegimenez <agusegimenez@gmail.com>
2024-06-23 01:26:56 -0300

    pusheo cambios porque tengo que intentar algo antes

--- Codigo/src/vistas/AtravesarCinturonVista.java
+++ Codigo/src/vistas/AtravesarCinturonVista.java
@@ -1,7 +1,28 @@
 package vistas;
 
+import controller.JugadorController;
+import controller.MapaController;
+
 import javax.swing.*;
 
 public class AtravesarCinturonVista extends JFrame {
+    private JPanel rootpane;
+    private JTextArea textCinturon;
+
+    public  AtravesarCinturonVista(){
+        super("Atravesar Cinturon");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(400,400);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        JTextArea poderCinturon = new JTextArea();
+        JugadorController controllerJugador = new JugadorController();
+        MapaController controllerMapa = new MapaController();
+
+        textCinturon.append("El poder del cinturon es: " + controllerMapa.getMapaEstelarView().);
+
+
+    }
 
 }
